0a9b9e4449a360bd840762ca104ecb8585d06472
Eddie Xu <50087436+EddieXu123@users.noreply.github.com>
2020-05-19 15:08:28 -0400

    Create PalindromicSubstrings.java

---
+++ PalindromicSubstrings.java
@@ -0,0 +1,21 @@
+public class PalindromicSubstrings {
+   public int countSubstrings(String str) {
+        if(str == null || str.length() < 1) return 0;
+        int count = 0;
+        for(int i=0;i<str.length();i++){
+            count += countPalindromes(str, i, i); //Count even sized
+            count += countPalindromes(str, i, i+1); //Count odd sized
+        }
+        return count;
+    }
+    
+    private int countPalindromes(String str, int s, int e){
+        int count = 0;
+        while(s>=0 && e<str.length() && str.charAt(s) == str.charAt(e)){
+            s--;
+            e++;
+            count++;
+        }
+        return count;
+    }
+}
