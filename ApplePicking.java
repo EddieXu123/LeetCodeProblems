d452ca60f4be11931c33849a1499fe3d695644c3
Eddie Xu <50087436+EddieXu123@users.noreply.github.com>
2020-06-04 23:29:43 -0400

    Update ApplePicking.java

--- ApplePicking.java
+++ ApplePicking.java
@@ -1,18 +1,18 @@
 class ApplePicking {
     public int totalFruit(int[] tree) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        int result = 0;
-        int start = 0;
-        for (int end = 0; end < tree.length; end++) {
-            map.put(tree[end], map.getOrDefault(tree[end], 0) + 1);
-            while (map.size() > 2) {
-                map.put(tree[start], map.get(tree[start]) - 1);
-                if (map.get(tree[start]) == 0) map.remove(tree[start]);
-                start++;
+        int max = 0;
+        int start = 0; // Start of sliding window
+        for (int end = 0; end < tree.length; end++) { // End of sliding window
+            map.put(tree[end], map.getOrDefault(tree[end], 0) + 1); // Counting frequency of fruits in basket
+            while (map.size() > 2) { // While we are unable to put anymore fruits in the basket
+                map.put(tree[start], map.get(tree[start]) - 1); // Decrease the first fruit by one
+                if (map.get(tree[start]) == 0) map.remove(tree[start]); // If the fruit is gone, then remove from the map
+                start++; // Everytime the first fruit is decreased, we increase (count how many of the first fruit we had in our basket)
             }
             
-            result = Math.max(result, end - start + 1);
+            max = Math.max(max, end - start + 1); // Return max
         }
-        return result;
+        return max;
     }
 }
