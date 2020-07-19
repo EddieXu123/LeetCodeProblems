3ef70a8f031423cec49e7e6d17d294da45ce8190
Eddie Xu <50087436+EddieXu123@users.noreply.github.com>
2020-07-18 23:42:59 -0400

    Update CourseScheduling.java

--- CourseScheduling.java
+++ CourseScheduling.java
@@ -1,35 +1,32 @@
 class CourseScheduling {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] inDegree = new int[numCourses];
         int counter = 0;
+        int[] inDegree = new int[numCourses]; // Array to hold onto the number of indegree a node has
         
         for (int i = 0; i < prerequisites.length; i++) {
-            inDegree[prerequisites[i][0]]++;
+            inDegree[prerequisites[i][0]]++; // If a node has a prerequisite, increment that node
         }
         
-        LinkedList<Integer> stack = new LinkedList<Integer>();
+        LinkedList<Integer> stack = new LinkedList<>();
         
         for (int i = 0; i < inDegree.length; i++) {
-            if (inDegree[i] == 0) {
-                stack.push(i);
-            }
+            // We want to start with nodes with no prerequistes. Also, since the classes are labeled 0 to numCourses-1, we know this will be ordered
+            if (inDegree[i] == 0) stack.push(i); // If a node does not have a prerequiste, push that into stack
         }
         
         while (!stack.isEmpty()) {
-            int current = stack.poll();
-            counter++;
-            
-            for (int i = 0; i < prerequisites.length; i++) {
-                if (current == prerequisites[i][1]) {
-                    inDegree[prerequisites[i][0]]--;
-                    if (inDegree[prerequisites[i][0]] == 0) {
-                        stack.push(prerequisites[i][0]);
+            counter++; // Increment counter to see if we have "taken" all the classes
+            int current = stack.poll(); // Take out the class 
+            for (int i = 0; i < prerequisites.length; i++) { // Going through original classes
+                if (current == prerequisites[i][1]) { // If the original class's prereq is the one we currently have taken
+                    inDegree[prerequisites[i][0]]--; // We can take away one OUTdegree
+                    if (inDegree[prerequisites[i][0]] == 0) { // If another class has no indegree (because we take away the OUTdegree from another class potentially going into that class)
+                        stack.push(prerequisites[i][0]); // Push that class into stack
                     }
                 }
             }
         }
         
-        return numCourses == counter;
-                                        
+        return counter == numCourses;
     }
 }
