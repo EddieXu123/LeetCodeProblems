b71de5121bef19a778f9ed5eb126c98dc61ea0a2
Eddie Xu <50087436+EddieXu123@users.noreply.github.com>
2020-08-12 10:30:18 -0400

    Update PascalsTriangle2.java

--- PascalsTriangle2.java
+++ PascalsTriangle2.java
@@ -18,4 +18,20 @@ class PascalsTriangle2 {
         result.add(1);
         return helperList(result, rowIndex - 1);
     }
+
+
+
+    // O(K) Solution
+    public static List<Integer> getRow2(int rowIndex) {
+        List<Integer> ret = new ArrayList<Integer>();
+        ret.add(1);
+        for (int i = 1; i <= rowIndex; i++) {
+            for (int j = i - 1; j >= 1; j--) {
+                int tmp = ret.get(j - 1) + ret.get(j);
+                ret.set(j, tmp);
+            }
+            ret.add(1);
+        }
+        return ret;
+    }  
 }
